27f8a1e75087ab91b3dadd6ae95e908f3fbfe473
yanzhiming <yanzhiming0211@163.com>
2020-08-31 00:57:41 +0800

    add 差分数组模板详解

--- 1109.航班预订统计.c
+++ 1109.航班预订统计.c
@@ -20,8 +20,16 @@ void Init() {
     }
 }
 
+void inSert(int l, int r,int c)
+{
+    g_flite[l] += c;
+    g_flite[r + 1] -= c;
+}
+
 int* corpFlightBookings(int** bookings, int bookingsSize, int* bookingsColSize, int n, int* returnSize){
-    //Init();
+
+    //构建差分数组g_flite只需要遍历插入inSert(i,i,res[i])即可
+    // 此题由于原来的数组res都为0，因此差分数组都为0，此时不需要构建差分数组
     memset(g_flite, 0, sizeof(g_flite));
     int*res = (int *)malloc(n * sizeof(int));
     if (res == NULL) {
@@ -29,9 +37,10 @@ int* corpFlightBookings(int** bookings, int bookingsSize, int* bookingsColSize, 
     }
     memset(res, 0, sizeof(int));
     for (int i = 0; i < bookingsSize; i++) {
-        g_flite[bookings[i][0]] += bookings[i][2];
-        g_flite[bookings[i][1] + 1] -= bookings[i][2];
+        inSert(bookings[i][0], bookings[i][1], bookings[i][2]);
     }
+
+    //求差分数组的前缀和就是原来的数组
     res[0] = g_flite[1];
     for (int j = 1; j < n; j++){
         res[j] =res[j - 1] + g_flite[j + 1];
